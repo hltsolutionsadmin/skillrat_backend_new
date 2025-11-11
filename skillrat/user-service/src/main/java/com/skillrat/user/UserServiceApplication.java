35f7cbb1193a60db6211b04c1e04bb471aa07594
pavanhlt7 <pavan@hltsolutions.com>
2025-11-11 15:38:46 +0530

    resloving conflists

--- skillrat/user-service/src/main/java/com/skillrat/user/UserServiceApplication.java
+++ skillrat/user-service/src/main/java/com/skillrat/user/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.skillrat.user.domain.User;
 import com.skillrat.user.domain.Employee;
@@ -20,10 +21,17 @@ import com.skillrat.user.repo.UserSkillRepository;
 import com.skillrat.user.repo.EducationRepository;
 import com.skillrat.user.repo.TitleRecordRepository;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EntityScan(basePackageClasses = {User.class, Employee.class, Role.class, ProfileExperience.class, Education.class, UserSkill.class, TitleRecord.class})
-@EnableJpaRepositories(basePackageClasses = {UserRepository.class, RoleRepository.class, ProfileExperienceRepository.class, UserSkillRepository.class, EducationRepository.class, TitleRecordRepository.class})
+@EntityScan(basePackageClasses = {
+        User.class, Employee.class, Role.class, ProfileExperience.class,
+        Education.class, UserSkill.class, TitleRecord.class
+})
+@EnableJpaRepositories(basePackageClasses = {
+        UserRepository.class, RoleRepository.class, ProfileExperienceRepository.class,
+        UserSkillRepository.class, EducationRepository.class, TitleRecordRepository.class
+})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
