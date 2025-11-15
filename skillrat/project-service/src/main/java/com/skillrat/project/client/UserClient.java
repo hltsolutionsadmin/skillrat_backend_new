58d63e327804bc266e536d6f5286ee9f79b38ca8
JT <jt@Juvaryas-MacBook-Air.local>
2025-11-15 15:28:09 +0530

    adde code

---
+++ skillrat/project-service/src/main/java/com/skillrat/project/client/UserClient.java
@@ -0,0 +1,19 @@
+
+package com.skillrat.project.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@FeignClient(
+        name = "user-service",
+        url = "${services.user.base-url:http://localhost:8082}",
+        path = "/api/users",
+        configuration = com.skillrat.project.config.FeignAuthConfig.class
+)
+public interface UserClient {
+
+    @GetMapping("/me")
+    Map<String, Object> me();
+}
