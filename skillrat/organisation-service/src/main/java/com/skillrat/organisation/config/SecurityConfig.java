9d1a4321fdc3f087cfca0248d15537ee0bb05e14
JT <jt@Juvaryas-MacBook-Air.local>
2025-11-18 18:39:03 +0530

    test

--- skillrat/organisation-service/src/main/java/com/skillrat/organisation/config/SecurityConfig.java
+++ skillrat/organisation-service/src/main/java/com/skillrat/organisation/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/b2b/test").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> oauth
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter())))
